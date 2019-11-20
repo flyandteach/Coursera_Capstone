18befb1d37cd6b304888559b8ab9ff2b0004bc89
flyandteach <53960669+flyandteach@users.noreply.github.com>
2019-11-19 20:15:34 -0800

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#This notebook will be used for creating the Coursera Capstone project
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
